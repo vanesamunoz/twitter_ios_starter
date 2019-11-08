595fc6b6ec15e6301259ffa5dbb2acae791a8f89
Vanesa Munoz Cuevas <nesa@live.unc.edu>
2019-11-07 20:52:53 -0500

    completed twitter app part 2

--- Twitter/HomeTableViewController.swift
+++ Twitter/HomeTableViewController.swift
@@ -33,6 +33,12 @@ class HomeTableViewController: UITableViewController {
         if let imageData = data {
             cell.profileImageView.image = UIImage(data: imageData)
         }
+        cell.setFavorite(tweetArray[indexPath.row]["favorited"] as! Bool)
+        cell.tweetId = tweetArray[indexPath.row]["id"] as! Int
+        //cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        cell.setRetweeted(tweetArray[indexPath.row]["retweeted"] as! Bool)
+        
+        
         
         return cell
     }
@@ -93,7 +99,11 @@ class HomeTableViewController: UITableViewController {
         loadTweets()
         myRefreshControl.addTarget(self, action: #selector(loadTweets), for: .valueChanged)
         tableView.refreshControl = myRefreshControl
+       
+        
+        
     }
+    
 
     // MARK: - Table view data source
 
@@ -106,5 +116,7 @@ class HomeTableViewController: UITableViewController {
         // #warning Incomplete implementation, return the number of rows
         return tweetArray.count
     }
+    
+    
 
 }
